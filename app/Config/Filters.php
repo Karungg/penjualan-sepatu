5ac76366eecd8f61f7b7eafb75cf4d5ce2795bf1
Karungg <miftahfadilah71@gmail.com>
2023-12-27 08:45:22 +0700

    setup myth library

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -25,6 +25,9 @@ class Filters extends BaseConfig
         'honeypot'      => Honeypot::class,
         'invalidchars'  => InvalidChars::class,
         'secureheaders' => SecureHeaders::class,
+        'login'      => \Myth\Auth\Filters\LoginFilter::class,
+        'role'       => \Myth\Auth\Filters\RoleFilter::class,
+        'permission' => \Myth\Auth\Filters\PermissionFilter::class,
     ];
 
     /**
@@ -36,8 +39,9 @@ class Filters extends BaseConfig
      */
     public array $globals = [
         'before' => [
-            // 'honeypot',
-            // 'csrf',
+            'honeypot',
+            'login',
+            'csrf',
             // 'invalidchars',
         ],
         'after' => [
